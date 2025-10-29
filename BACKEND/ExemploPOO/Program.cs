87526a25838fabd04f2731d5c3b3666ff311a81e
Marcelo Santiago <53163824+MrMaicke@users.noreply.github.com>
2025-10-28 21:56:40 -0300

    Add files via upload

---
+++ BACKEND/ExemploPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class Calculadoras
+{
+    // Propriedade (Atributos)
+    public float numero01;
+    public float numero02;
+
+    // Métodos (Comportamentos)
+    public float Somar()
+    {
+        return numero01 + numero02;
+    }
+
+    public float Subtrair()
+    {
+        return numero01 - numero01;
+    }
+    public float dividir()
+    {
+        return numero01 / numero01;
+    }
+    public float multiplicar()
+    {
+        return numero01 * numero01;
+    }
+}
