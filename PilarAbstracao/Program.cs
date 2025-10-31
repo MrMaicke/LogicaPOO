2e4b68996d28beec305dba0fbec10509de777a5c
Marcelo Santiago <53163824+MrMaicke@users.noreply.github.com>
2025-10-30 21:37:01 -0300

    Add files via upload

---
+++ PilarAbstracao/Program.cs
@@ -0,0 +1,13 @@
+﻿public abstract class FomaGeometrica
+{
+    public decimal Area;
+}
+
+// Configuaração Padrão
+class Program
+{
+    static void Main()
+    {
+        FomaGeometrica forma = 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,162 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// CamalCase = nomeVariavel
-// PascalCase = NomeVariavel
-//snack_case = nome_variavel
-
-//tipoDeDado nomeVariavel = valorDaVariavel
-string nome = "Marcelo Santiago de Oliveira";
-
-int numeroInteiro = 1;
-
-double preco = 5.60D;
-
-float Altura = 1.80f;
-
-bool boleanos = true;
-
-char character = 'A';
-
-Console.WriteLine(nome);
-
-/*----------------------------------------*/
-
-Console.WriteLine("Soma :" + (1 + 2));
-Console.WriteLine("Subtração :" + (1 - 2));
-Console.WriteLine("Divisão :" + (1 / 2));
-Console.WriteLine("Multiplicação :" + (1 * 2));
-/*----------------------------------------*/
-
-
-// = atribuição
-//== comparação
-//=== comparar valor e tipo
-
-// Operador de igualdade (==)
-// Comparar se valores são iguais
-Console.WriteLine(5 == 5);
-
-// Operador de diferença (!=)
-// Comparar se valores não são iguais
-Console.WriteLine(5 != 5);
-
-// Operador de maior (>)
-// Comparar se um valor é maior do que o outro
-Console.WriteLine(8 > 4);
-
-// Operador de menor que (<)
-// Comparar se um valor é menor que o outro
-Console.WriteLine(4 < 8);
-
-// Operador de maior ou igual (>=)
-// comparar se um valor é maior ou igual
-Console.WriteLine(8 >= 4);
-
-// Operador de menor ou igual (<=)
-// comparar se um valor é maior ou igual
-Console.WriteLine(4 <= 8);
-
-
-/*----------------------------------------*/
-
-/* OPERADORES LÓGICOS */
-// && (e) -- || (ou)
-// ! (não/negativa)
-
-Console.WriteLine(5 == 5 && 8 == 8); // true && true (true)
-
-Console.WriteLine(5 == 5 && 18 == 8); // true && false (false)
-
-Console.WriteLine(5 == 15 && 18 == 8); // false && false (false)
-
-
-Console.WriteLine(2 == 2 || 3 == 3); // true || true (true)
-
-Console.WriteLine(2 == 4 || 3 == 3); // false || true (true)
-
-Console.WriteLine(2 == 4 || 3 == 9); // false || false (false)
-
-
-console.WriteLine(!true == true);
-
-console.WriteLine(!(2 == 2 || 3 == 3));
-
-/*----------------------------------------*/
-
-bool noite = true;
-
-//se naose
-if(noite){
-    console.WriteLine("Agora é noite");
-}
-else{
-    console.WriteLine("Agora é de dia");
-}
-
-//if ternario
-var condicao = (noite) ? "Agora é de noite!" : "Agora é de dia";
-
-var idade = 20;
-
-if (idade < 18 ){
-    console.WriteLine("Pessoa menor de idade");
-}
-else if(idade >= 18 && idade < 21){
-    console.WriteLine("Maior de idade mas não de maioridade penal");
-}
-else
+﻿public class Veiculo
 {
-    console.WriteLine("Maior de idade legal");
+    public string Marca;
+
+    public void LigarMotor()
+    {
+        Console.WriteLine("Veiculo ligado!")
+    }
 }
 
-switch(diaSemana)
+// Classe carro vai herdar os comportamentos e propriedades de veiculo
+public class Carro : Veiculo
 {
-    case 1:
-        Console.WriteLine("Domingo");
-        break;
-    case 2:
-        Console.WriteLine("Segunda-feira");
-        break;
-    case 3:
-        Console.WriteLine("Terça-feira");
-        break;
-    case 4:
-        Console.WriteLine("Quarta-feira");
-        break;
-    case 5:
-        Console.WriteLine("Quinta-feira");
-        break;
-    case 6:
-        Console.WriteLine("Sexta-feira");
-        break;
-    case 7:
-        Console.WriteLine("Sabado-feira");
-        break;
-    default:
-        Console.WriteLine("Dia da semana invalido");
-        break;
+    public int quantidadePortas;
 }
 
-/*----------------------------------------*/
-
-/* Enquanto == While -> Laço de repetição */
-int numero = 1;
-
-while (numero < 10)
+public class Moto : Veiculo
 {
-    console.WriteLine(numero);
-    numero = numero + 1; //numero++
+    public int cilindradas;
 }
 
-int numero02 = 1;
+/* Configurações do projeto */
+class Program
+{
+    static void Main()
+    {
+        //Criando uma instancia da classe Carro
+        Carro meuCarro = new Carro();
 
-do{
-    console.WriteLine(numero02);
-    numer02 = numero02 + 1; // numero++
-}while (numero02 < 10);
+        meuCarro.quantidadePortas = 2;
+        meuCarro.Marca = "Honda Civic";
+        meuCarro.LigarMotor();
 
-/*for(valor inicial; condição; processamento;)*/
-for(int valor = 0; valor < 10; valor++){
-    console.WriteLine(valor);
+        Moto minhaMoto = new Moto();
+
+        minhaMoto.cilindradas = 150;
+        minhaMoto.Marca = "Harley";
+        minhaMoto.LigarMotor();
+    } 
 }
